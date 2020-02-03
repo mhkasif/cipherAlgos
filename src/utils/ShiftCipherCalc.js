8d50cf8c648b485ae812f3ebbeb384283d021a30
mhkasif <haseebasif97@gmail.com>
2020-02-04 02:25:48 +0500

    shift cipher bug remve

--- src/utils/ShiftCipherCalc.js
+++ src/utils/ShiftCipherCalc.js
@@ -20,16 +20,19 @@ export const shiftCipherDecryptCalc = (text, key) => {
   const textArray = text.split("");
   let cipherTextArray = "";
   for (var i = 0; i < text.length; i++) {
-    if (alphabetsArray.indexOf(textArray[i]) - Number(key) < 0) {
+    if (textArray[i] === " ") {
+
+      cipherTextArray += " ";
+
+  }
+  else if (alphabetsArray.indexOf(textArray[i]) - Number(key) < 0) {
       const newChar =
         (alphabetsArray.indexOf(textArray[i]) - Number(key) + 26) % 26;
       cipherTextArray += alphabetsArray[newChar];
     } else if (textArray[i] !== " ") {
       const newChar = (alphabetsArray.indexOf(textArray[i]) - Number(key)) % 26;
       cipherTextArray += alphabetsArray[newChar];
-    } else {
-      cipherTextArray += " ";
-    }
+    } 
   }
 
   return cipherTextArray;
